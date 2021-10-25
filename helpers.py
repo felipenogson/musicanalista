c8dbde476e9a34d164dc9be44a81e935a25e2f12
Dell Home <pon.felipon@gmail.com>
2021-10-25 09:37:38 -0700

    cleaning helpers.py and adding responsivnes to tables

--- helpers.py
+++ helpers.py
@@ -23,9 +23,7 @@ def get_musicbrainz_info(pd_query:dict):
     releases = musicbrainzngs.search_releases(pd_query['Album'], date=pd_query['Release Date'], artist=pd_query['Artist Name'])
     eureka = {}
     for release in releases['release-list']:
-        print('voy a ir a buscar...')
         release_query = musicbrainzngs.get_release_by_id(release['id'], includes=['recordings'])
-        print('ya regrese')
 
         if 'cover-art-archive' in release_query['release']:
             artwork = release_query['release']['cover-art-archive']['artwork']
@@ -33,7 +31,6 @@ def get_musicbrainz_info(pd_query:dict):
                 image_list = musicbrainzngs.get_image_list(release['id'])
                 for i in image_list['images']:
                     if (i['approved'] == True) and (i['front'] == True):
-                        print('Intenta esta: ', i['image'])
                         pd_query['release_id'] = release['id']
                         pd_query['cover_art_url'] = i['image']
                         eureka = release_query
@@ -42,12 +39,10 @@ def get_musicbrainz_info(pd_query:dict):
                     break
 
             else:
-                print('La que sigue..')
                 continue
 
     track_list=[]
     for song in eureka['release']['medium-list'][0]['track-list']:
-        print(song['position'],song['recording']['title'])
         track_list.append({'position': song['position'], 'title':song['recording']['title']})
     pd_query['track_list'] = track_list
 
